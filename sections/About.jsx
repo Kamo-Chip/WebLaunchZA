8f767a9fd2e03c48626322762b74476407116821
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2022-12-09 12:09:22 +0200

    Added links to about

--- sections/About.jsx
+++ sections/About.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const About = () => {
   useEffect(() => {
@@ -35,12 +36,12 @@ const About = () => {
             {" consumer's"} attention?
           </p>
           <p>
-            At MakersDev our mission is to help small to medium sized businesses secure their
-            growth and profitability in the ever-changing digital economy. We do
-            this by establishing your online presence with ease, at an
-            affordable price. We understand how hard it is to get started, so we
-            cut out the fluff of traditional web development agencies by making
-            you pay for only what you need.
+            At MakersDev our mission is to help small to medium sized businesses
+            secure their growth and profitability in the ever-changing digital
+            economy. We do this by establishing your online presence with ease,
+            at an affordable price. We understand how hard it is to get started,
+            so we cut out the fluff of traditional web development agencies by
+            making you pay for only what you need.
           </p>
           <p>
             Our range of services makes it easy for you to get started on the
@@ -48,13 +49,27 @@ const About = () => {
             for your business and make your business rank high in searches.
           </p>
           <p>
-            Having a website and a Google Business Profile builds trust between
-            you and potential clients because it lets them know that you are
-            legit. This increases the chances of them becoming paying customers.
-            Our services have proven to be an extremely effective way of
-            improving your visibility, and we can confidently say that we will
-            make you seen! So let us handle the hassle of putting your business
-            online so that you can focus on doing what you do best!
+            Having a{" "}
+            <Link
+              href="/blog/posts/why-your-business-needs-a-website"
+              style={{ color: "rgb(0, 106, 255)" }}
+            >
+              website
+            </Link>{" "}
+            and a{" "}
+            <Link
+              href="/blog/posts/how-to-advertise-on-google-for-free"
+              style={{ color: "rgb(0, 106, 255)" }}
+            >
+              Google Business Profile
+            </Link>{" "}
+            builds trust between you and potential clients because it lets them
+            know that you are legit. This increases the chances of them becoming
+            paying customers. Our services have proven to be an extremely
+            effective way of improving your visibility, and we can confidently
+            say that we will make you seen! So let us handle the hassle of
+            putting your business online so that you can focus on doing what you
+            do best!
           </p>
         </div>
       </div>
